b42fb7e4608ab37a57e3f0a097e8f69edf554e6d
IslamHassan94 <islamhasan@hotmail.com>
2022-12-25 18:11:26 +0200

    Adding pug as templating engine

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const bodyParser = require("body-parser");
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+app.listen(3000, () => {
+  console.log("Server runnung on port 3000");
+});
