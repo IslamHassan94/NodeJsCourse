324621e877a15d481d6909cc9a1f93addf41fb2d
IslamHassan94 <islamhasan@hotmail.com>
2023-01-14 14:09:10 +0200

    Added associations among tables

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -20,6 +23,18 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000, () => {
-  console.log("Server Listening to port 3000");
-});
+// This means if a user is deleted also any related products will be deleted
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
+sequelize
+  .sync({ force: true })
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000, () => {
+      console.log("Server Listening to port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
